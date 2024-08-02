cd7c7bfea64bad689072349dc3956cb3249936b7
yyhis <oouoo717@gmail.com>
2024-08-02 12:29:30 +0900

    Init: revieworder

---
+++ src/main/java/net/yyhis/revieworder/RevieworderApplication.java
@@ -0,0 +1,13 @@
+package net.yyhis.revieworder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RevieworderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RevieworderApplication.class, args);
+	}
+
+}
---
+++ src/test/java/net/yyhis/revieworder/RevieworderApplicationTests.java
@@ -0,0 +1,13 @@
+package net.yyhis.revieworder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RevieworderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
