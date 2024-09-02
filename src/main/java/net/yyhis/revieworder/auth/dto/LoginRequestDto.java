30302fc49351753452c3ff89ce303e637538bb95
yyhis <oouoo717@gmail.com>
2024-09-02 17:15:11 +0900

    feat: add authentication DTOs

---
+++ src/main/java/net/yyhis/revieworder/auth/dto/LoginRequestDto.java
@@ -0,0 +1,11 @@
+package net.yyhis.revieworder.auth.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class LoginRequestDto {
+    private String username;
+    private String password;
+}
